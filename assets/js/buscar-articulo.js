fefbe5ad5a7429827d012e9b3a6be4dde0e3faa9
Carolina Uribe Botero <uribebotero.carolin@gmail.com>
2023-03-28 22:22:09 -0500

    Documentar JavaScript

--- assets/js/buscar-articulo.js
+++ assets/js/buscar-articulo.js
@@ -3,15 +3,18 @@ const buscador = document.querySelector("#buscador");
 const cards = document.querySelectorAll(".card");
 const errorBuscador = document.querySelector("#error_buscador");
 
+// Buscar la info seleccionada
 buscador.addEventListener("click", (e) => {
   e.preventDefault(); // previene que se recarge la pagina
   const valorBusqueda = buscador.value.toLowerCase();
 
+  // Recorrer los articulos y buscar el valor seleccionado
   for (let i = 0; i < cards.length; i++) {
     const card = cards[i];
     const texto = card.textContent.toLocaleLowerCase();
     let resultadoEncontrado = false;
 
+    // hacer si el valor incluye la opcion seleccionada
     if (texto.includes(valorBusqueda)) {
       card.style.display = "block";
       resultadoEncontrado = true;
